46d27584b16b3314f87a3eb14816f6bf27201660
Matt Bryant <brybrant@gmail.com>
2024-11-05 09:02:22 +0000

    Fix eslint. Swap sass to sass-embedded. Update config.

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,7 @@
+import { For } from 'solid-js';
 import { render } from 'solid-js/web';
 
-import GitHubSVG from '../node_modules/@brybrant/svg-icons/GitHub.svg?component-solid';
+import GitHubSVG from '../node_modules/@brybrant/svg-icons/GitHub.svg';
 
 import './app.scss';
 import './kaleidoscope.scss';
@@ -10,32 +11,37 @@ function App() {
     <>
       <div class='background__container'>
         <div class='background'>
-          <For each={Array(12)}>{() =>
-            <div class='a'>
-              <div class='b'>
-                <div class='wedges'/>
-                <div class='pills'/>
-                <div class='squares'/>
-                <div class='petals'/>
+          <For each={Array(12)}>
+            {() => (
+              <div class='a'>
+                <div class='b'>
+                  <div class='wedges' />
+                  <div class='pills' />
+                  <div class='squares' />
+                  <div class='petals' />
+                </div>
               </div>
-            </div>
-          }</For>
+            )}
+          </For>
         </div>
       </div>
       <main>
         <h1>
-          <For each={'KALEIDOSCOPE'}>{(letter, i) =>
-            <span class={`letter-${i()}`}>{letter}</span>
-          }</For>
+          <For each={'KALEIDOSCOPE'}>
+            {(letter, i) => <span class={`letter-${i()}`}>{letter}</span>}
+          </For>
         </h1>
         <a
           class='button'
           href='https://github.com/brybrant/kaleidoscope'
           target='_blank'
-        ><GitHubSVG/>View Source</a>
+        >
+          <GitHubSVG />
+          View Source
+        </a>
       </main>
     </>
   );
 }
 
-render(() => <App/>, document.getElementById('app'))
+render(() => <App />, document.getElementById('app'));
--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,51 @@
 import { defineConfig } from 'vite';
 import eslintPlugin from 'vite-plugin-eslint2';
 import solidPlugin from 'vite-plugin-solid';
-import stylelintPlugin from 'vite-plugin-stylelint';
 import solidSvgPlugin from 'vite-plugin-solid-svg';
+import stylelintPlugin from 'vite-plugin-stylelint';
 
 import * as configs from '@brybrant/configs';
 
-export default defineConfig({
-  base: '/kaleidoscope/',
-  css: {
-    postcss: configs.postCSSConfig,
-  },
-  plugins: [
-    stylelintPlugin({
-      lintInWorker: true,
-      config: configs.stylelintConfig,
-    }),
-    solidPlugin(),
-    solidSvgPlugin({
-      svgo: {
-        enabled: true,
-        svgoConfig: configs.svgoConfig,
+export default defineConfig(({ mode }) => {
+  const development = mode === 'development';
+
+  return {
+    base: '/kaleidoscope/',
+    build: {
+      minify: development ? true : 'terser',
+      ...(!development && {
+        terserOptions: configs.terserConfig,
+      }),
+    },
+    css: {
+      postcss: configs.postCSSConfig,
+      preprocessorOptions: {
+        scss: {
+          api: 'modern-compiler',
+        },
       },
-    }),
-    eslintPlugin({
-      lintInWorker: true,
-    }),
-  ],
-  server: {
-    host: '127.0.0.1',
-    port: 3000,
-    strictPort: true,
-  },
+    },
+    plugins: [
+      stylelintPlugin({
+        lintInWorker: true,
+        config: configs.stylelintConfig,
+      }),
+      solidPlugin(),
+      solidSvgPlugin({
+        defaultAsComponent: true,
+        svgo: {
+          enabled: true,
+          svgoConfig: configs.svgoConfig,
+        },
+      }),
+      eslintPlugin({
+        lintInWorker: true,
+      }),
+    ],
+    server: {
+      host: '127.0.0.1',
+      port: 3000,
+      strictPort: true,
+    },
+  };
 });
